95361c520911f7ae7b221264b391f5192c9c9129
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-04-14 23:55:06 -0400

    Added canvas logic for UIView

--- ActivityApp/Canvas.swift
+++ ActivityApp/Canvas.swift
@@ -7,3 +7,64 @@
 //
 
 import UIKit
+
+class Canvas: UIView {
+    
+    private var strokeColor = UIColor.black
+    private var strokeWidth: Float = 1
+    
+    func setStrokeWidth(width: Float) {
+        self.strokeWidth = width
+    }
+    
+    func setStrokeColor(color: UIColor) {
+        self.strokeColor = color
+    }
+    
+    func undo() {
+        _ = lines.popLast()
+        setNeedsDisplay()
+    }
+    
+    func clear() {
+        lines.removeAll()
+        setNeedsDisplay()
+    }
+    
+    private var lines = [Line]()
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        lines.forEach { (line) in
+            context.setStrokeColor(line.color.cgColor)
+            context.setLineWidth(CGFloat(line.strokeWidth))
+            context.setLineCap(.round)
+            for (i, p) in line.points.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+            context.strokePath()
+        }
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append(Line.init(strokeWidth: strokeWidth, color: strokeColor, points: []))
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else { return }
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.points.append(point)
+        lines.append(lastLine)
+        setNeedsDisplay()
+    }
+    
+}
