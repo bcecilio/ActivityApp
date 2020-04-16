3c6b3bd09e7d08c110471b4555e2ffb3f686ba63
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-04-16 18:22:20 -0400

    added files to Jaheed folder

--- ActivityApp/Line.swift
+++ ActivityApp/Line.swift
@@ -2,11 +2,12 @@
 //  Line.swift
 //  ActivityApp
 //
-//  Created by Jaheed Haynes on 4/14/20.
+//  Created by Jaheed Haynes on 4/16/20.
 //  Copyright © 2020 Brendon Cecilio. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
 struct Line {
     let strokeWidth: Float
