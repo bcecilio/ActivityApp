c71b762ceaa2db06ebc1e49c75a6a49ead63e031
Eric Davenport <EricDavenport@pursuit.org>
2020-04-17 07:54:30 -0400

    Core Data set up complete, saving both photos. Issues loading back after on feed view. View avtivity complete

--- ActivityApp/EricViewController.swift
+++ ActivityApp/EricViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class EricViewController: UIViewController {
-
+  
   @IBOutlet weak var photographerImageView: UIImageView!
   @IBOutlet weak var userImageView: UIImageView!
   
@@ -20,13 +20,17 @@ class EricViewController: UIViewController {
     pickerController.delegate = self
     return pickerController
   }()
+  
+  private var photographerImage: Data?
+  private var userImage: Data?
+  
   override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
+    super.viewDidLoad()
     
-
+    
+  }
+  
+  
   @IBAction func photographerButtonPressed(_ sender: UIButton) {
     imagePickerController.sourceType = .camera
     present(imagePickerController, animated: true)
@@ -38,6 +42,15 @@ class EricViewController: UIViewController {
   }
   
   @IBAction func submitButtonPressed(_ sender: UIButton) {
+    let newObject = CoreDataManager.shared.createMediaObject(photographerImage, secondImage: userImage, title: "Strike A Pose, Twice")
+    print("object created of type \(newObject.title ?? "no title")")
+    guard let data2 = newObject.image,
+    let data = newObject.video else {
+        return
+    }
+    userImageView.image = UIImage(data: data2)
+    photographerImageView.image = UIImage(data: data)
+    
   }
 }
 
@@ -45,17 +58,19 @@ extension EricViewController: UIImagePickerControllerDelegate, UINavigationContr
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     if imagePickerController.sourceType == .camera {
       if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
-      let imageData = originalImage.jpegData(compressionQuality: 1.0) {
-      photographerImageView.image = originalImage
-      print("photoGrapher image set")
+        let imageData = originalImage.jpegData(compressionQuality: 1.0) {
+        photographerImageView.image = originalImage
+        photographerImage = imageData
+        print("photoGrapher image set")
       }
       
     } else if imagePickerController.sourceType == .photoLibrary {
       if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
-           let imageData = originalImage.jpegData(compressionQuality: 1.0) {
-           userImageView.image = originalImage
-           print("userImage image set")
-           }
+        let imageData = originalImage.jpegData(compressionQuality: 1.0) {
+        userImageView.image = originalImage
+        userImage = imageData
+        print("userImage image set")
+      }
     }
     imagePickerController.dismiss(animated: true)
   }
