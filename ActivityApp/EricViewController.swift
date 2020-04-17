fb81d3590d166de506fa24d3f7e0f0c0a9b7a5e2
Eric Davenport <EricDavenport@pursuit.org>
2020-04-17 05:38:32 -0400

    View controller set up - adds phots from library and camera

--- ActivityApp/EricViewController.swift
+++ ActivityApp/EricViewController.swift
@@ -10,13 +10,54 @@ import UIKit
 
 class EricViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var photographerImageView: UIImageView!
+  @IBOutlet weak var userImageView: UIImageView!
+  
+  private lazy var imagePickerController: UIImagePickerController = {
+    let mediaType = UIImagePickerController.availableMediaTypes(for: .photoLibrary)
+    let pickerController = UIImagePickerController()
+    pickerController.mediaTypes = mediaType ?? ["kUTTypeImage"]
+    pickerController.delegate = self
+    return pickerController
+  }()
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
 
-    
-
+  @IBAction func photographerButtonPressed(_ sender: UIButton) {
+    imagePickerController.sourceType = .camera
+    present(imagePickerController, animated: true)
+  }
+  
+  @IBAction func panoramicButtonPressed(_ sender: UIButton) {
+    imagePickerController.sourceType = .photoLibrary
+    present(imagePickerController, animated: true)
+  }
+  
+  @IBAction func submitButtonPressed(_ sender: UIButton) {
+  }
 }
+
+extension EricViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if imagePickerController.sourceType == .camera {
+      if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
+      let imageData = originalImage.jpegData(compressionQuality: 1.0) {
+      photographerImageView.image = originalImage
+      print("photoGrapher image set")
+      }
+      
+    } else if imagePickerController.sourceType == .photoLibrary {
+      if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage,
+           let imageData = originalImage.jpegData(compressionQuality: 1.0) {
+           userImageView.image = originalImage
+           print("userImage image set")
+           }
+    }
+    imagePickerController.dismiss(animated: true)
+  }
+}
+
